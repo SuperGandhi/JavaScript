8b281aeabc2aac5cceb943f6f8e38cfe25779e6b
SuperGandhi <patrickborjabueno@hotmail.com>
2022-07-20 16:19:10 -0500

    Types primitives complete

---
+++ types-primitives/index.js
@@ -0,0 +1,34 @@
+// Types primitives
+
+// Number
+5
+
+// String
+"hi";
+'hi';
+
+// booleans
+
+true;
+false;
+
+// null and undefined
+
+null;
+undefined;
+
+/**
+ * What's the difference?
+ * null, undefined, false,0
+ */
+
+
+console.log('====================================');
+console.log(null == null);
+console.log('====================================');
+
+if(undefined){
+    console.log('yeah');
+}else{
+    console.log('not');
+}
