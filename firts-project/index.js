20834f78741cfdea5e3178f873f1025df755323f
SuperGandhi <patrickborjabueno@hotmail.com>
2022-07-20 15:02:29 -0500

    Exercise completed

--- firts-project/index.js
+++ firts-project/index.js
@@ -1,3 +1,3 @@
 console.log('====================================');
-console.log('Hi');
+console.log('Hi, this is my firts exercise with Node in the best programming Bootcamp in the world');
 console.log('====================================');
