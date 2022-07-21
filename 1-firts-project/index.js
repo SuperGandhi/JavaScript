ef1ddee6d93cc9a5b3d1b5368fabf04af3b7e938
SuperGandhi <patrickborjabueno@hotmail.com>
2022-07-21 13:07:17 -0500

    Complete

--- 1-firts-project/index.js
+++ 1-firts-project/index.js
@@ -1,3 +1,5 @@
 console.log('====================================');
 console.log('Hi, this is my firts exercise with Node in the best programming Bootcamp in the world');
 console.log('====================================');
+
+
