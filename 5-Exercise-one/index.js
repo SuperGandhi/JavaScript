3fff32d71e2eda1926053c221743e11bc93def77
SuperGandhi <patrickborjabueno@hotmail.com>
2022-07-22 15:57:08 -0500

    Exercise one complete

---
+++ 5-Exercise-one/index.js
@@ -0,0 +1,13 @@
+const name = "Patrick";
+const age = 20;
+const dev = true;
+const date = "2017-01-01";
+
+const bookFav = {
+    title : "Parmenides",
+    autor: "Plato",
+    date: 1800,
+    url : "https://www.buscalibre.pe/libro-parmenides/9788499201856/p/30909094"
+} 
+
+console.log(bookFav);
