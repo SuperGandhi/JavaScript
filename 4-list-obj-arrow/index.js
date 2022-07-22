794e53137349bd064b969c987174851e834c16da
SuperGandhi <patrickborjabueno@hotmail.com>
2022-07-22 15:50:56 -0500

    Complete

---
+++ 4-list-obj-arrow/index.js
@@ -0,0 +1,45 @@
+// Notación
+// ; --> Delimitar el final de una línea
+const b = 4;
+b + 4;
+
+// . --> Se utiliza en los objetos para acceder a los atributos
+// car.large
+
+// [] --> listas, arreglos o arrays
+const array = [1,2,3,4,5]
+console.log(array[2]);
+
+// () --> Funciones
+function suma(a,b){
+    // code here
+}
+
+// {} --> LLaves para objetos, funciones y estructuras de control 
+
+const car = {
+    large: 10,
+    height: 2,
+    brand: "Apple",
+    isBlack: true,
+    contacts: ["Rod", "Petri", "Row"],
+    hardware:{
+        brand: "Wolswagen",
+        space: 32
+    },
+    "height-car": 3
+}
+
+car.year= 2020;
+car.brand = "Kia"
+
+
+console.log(car.hardware);
+
+// if (true){
+//     // code here
+// }
+
+// Fechas
+const hour =new Date();
+console.log(righ);
