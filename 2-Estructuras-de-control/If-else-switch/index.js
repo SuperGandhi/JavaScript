93dec288794b9c4ae914e2a20199a95da7084125
SuperGandhi <patrickborjabueno@hotmail.com>
2022-07-23 15:44:18 -0500

    Complete

---
+++ 2-Estructuras-de-control/If-else-switch/index.js
@@ -0,0 +1,50 @@
+// Bifurcaciones if ... else
+
+// let money = 100;
+// let restante = 50;
+// if(money > restante){
+//     console.log("You have money");
+// }else{
+//     console.log("Don't have money")
+// }
+
+// If else + if else
+
+// let note = 10;
+
+// if(note === 10){
+//     console.log("F");
+// }else if(note>=12){
+//     console.log("Great")
+// }else if(note>=18){
+//     console.log("Excellent");
+// }else if (note === 20){
+//     console.log("Super");
+// }else{
+//     console.log("Entry your note");
+// }
+
+// Sentece Switch
+
+let note = 20;
+
+switch(note){
+    case 5:
+        console.log("F");
+        break;
+    case 4:
+        console.log("F");
+        break;
+    case 3:
+        console.log("F");
+        break;
+    case 2:
+        console.log("F");
+        break;
+    case 1:
+        console.log("F");
+        break;
+    default:
+        console.log("Error");
+        break;
+    }
