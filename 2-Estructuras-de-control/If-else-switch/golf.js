c8f54fbf3b493383064d7558cf1cb4d36bfb4732
SuperGandhi <patrickborjabueno@hotmail.com>
2022-08-03 14:22:05 -0500

    Complete

---
+++ 2-Estructuras-de-control/If-else-switch/golf.js
@@ -0,0 +1,20 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Cambia solo el código debajo de esta línea
+  if (strokes === 1) return "Hole-in-one!";
+
+  else if ((strokes - par) <= -2) return 'Eagle';
+
+  else if ((strokes - par) === -1) return 'Birdie';
+
+  else if (strokes === par) return 'Par';
+
+  else if ((strokes - par) === 1) return 'Bogey';
+
+  else if ((strokes - par) === 2) return 'Double Bogey';
+
+  else return 'Go Home!';
+}
+
+golfScore(5, 1);
