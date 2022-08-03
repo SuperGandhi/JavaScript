79eace43cf4189030441d24fb4f153d18b5fed7a
SuperGandhi <patrickborjabueno@hotmail.com>
2022-08-03 17:01:31 -0500

    Complete

---
+++ 6-Funciones/cargaSobrecarga.js
@@ -0,0 +1,18 @@
+function greeting(){
+    hi()
+}
+
+function hi(){
+    console.log('====================================');
+    console.log("Hi! I'm a function hi");
+    console.log('====================================');
+}
+
+greeting()
+
+// funciones recursivas 
+// function recursive(){
+//     recursive()
+// }
+
+// recursive();
