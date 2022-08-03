27cd15f8362e58d5cf3071b54ae55291fb846f83
SuperGandhi <patrickborjabueno@hotmail.com>
2022-08-02 23:57:37 -0500

    Complete

--- 6-Funciones/introFunc.js
+++ 6-Funciones/introFunc.js
@@ -25,4 +25,36 @@ function greetingPerson(obj){
     console.log(`Hi ${obj.name} ${obj.lastName}`);
 }
 
+// greetingPerson()
 
+
+function printNumber(n=7){ // Parámetros por defecto
+    console.log(n);
+}
+
+printNumber(10)
+
+function otherNumber(...props){
+    console.log(props);
+}
+otherNumber(1,2,3,4,5,6,7,8)
+
+
+function sum(...nums){
+    // el return se usa para que la función nos devuelva un resultado
+   return nums.reduce((a,b) => a + b);
+}
+
+const s = sum(1,23,3,4,56,6,2)
+console.log(s);
+
+let vari = "hi";
+
+function multiplicar(a=0, b = 0){
+    console.log(vari);
+    // let varInter = "Bye"
+    return a * b
+}
+
+console.log(varInter);
+console.log(multiplicar(2,10));
