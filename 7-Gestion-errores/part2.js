f8a4f484942a091014b7d2152a35b3ba29f4208f
SuperGandhi <patrickborjabueno@hotmail.com>
2022-08-06 13:42:41 -0500

    Complete

--- 7-Gestion-errores/part2.js
+++ 7-Gestion-errores/part2.js
@@ -1,4 +1,4 @@
-
+const logger = require('./logger');
 
 // console.log("Hi showing to the screen");
 // console.info("Hi I'm informative message");
@@ -6,24 +6,8 @@
 // console.debug("Hi I'm debug");
 // console.error("Hi I'm ERROR")
 
-const winston = require('winston');
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
-
-
-logger.log("Hi showing to the screen");
+// logger.log("Hi showing to the screen");
 logger.info("Hi I'm informative message");
 logger.warn("Hi is an warning");
 logger.debug("Hi I'm debug");
