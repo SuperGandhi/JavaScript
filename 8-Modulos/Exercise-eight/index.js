080e5aa0d100e131fd3b48f5b98d3de598da28f4
SuperGandhi <patrickborjabueno@hotmail.com>
2022-08-08 13:07:30 -0500

    Complete

---
+++ 8-Modulos/Exercise-eight/index.js
@@ -0,0 +1,8 @@
+import * as modulesMathe from '../Exercise-eight/ModulesMath/controller.js'
+import chalk from 'chalk';
+
+const multi = modulesMathe.multiplica(1,2)
+console.log(multi);
+
+const sum = modulesMathe.suma(4,5);
+console.log(chalk.green(sum));
