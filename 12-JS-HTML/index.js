c4305af81319bce14f76bceef25a46ca986ea811
SuperGandhi <patrickborjabueno@hotmail.com>
2022-08-12 15:08:46 -0500

    Complete

--- 12-JS-HTML/index.js
+++ 12-JS-HTML/index.js
@@ -1,12 +1,3 @@
-const a = 4
-const b = 8
-
-console.log('====================================');
-console.log(a * b);
-console.log('====================================');
-
-
-const p = document.getElementsById("parrafo");
-console.log('====================================');
-console.log(p);
-console.log('====================================');
+new TypeIt("#myElement", {
+    strings: "This is what will be typed!",
+  }).go();
