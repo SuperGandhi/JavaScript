4b37b5da914eac33a20ee6b9be94033f5bd8d4b9
SuperGandhi <patrickborjabueno@hotmail.com>
2022-08-12 14:34:50 -0500

    Complete

---
+++ 12-JS-HTML/index.js
@@ -0,0 +1,12 @@
+const a = 4
+const b = 8
+
+console.log('====================================');
+console.log(a * b);
+console.log('====================================');
+
+
+const p = document.getElementsById("parrafo");
+console.log('====================================');
+console.log(p);
+console.log('====================================');
