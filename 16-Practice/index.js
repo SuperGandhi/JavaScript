8a44bf139780921acb5ac157b6c8a1c6b3c507d6
SuperGandhi <patrickborjabueno@hotmail.com>
2022-08-19 16:07:50 -0500

    Complete

--- 16-Practice/index.js
+++ 16-Practice/index.js
@@ -5,8 +5,9 @@
  */
 
 // Example 1
+
 const oops = function () {
-    haha = 2;
+    haha = 2; // the variable was not declared
 
     console.log(haha);
 };
@@ -16,13 +17,15 @@ console.log(haha);
 
 // Example 2
 const outer = function () {
-    for (let i = 1; i <= 3; i++) {
+    for (let i = 1; i <= 3; i++) { // the variable was not declared
         inner();
     }
     // 1,2,3
 };
 const inner = function () {
-    for (let i = 1; i <= 5; i++) {
+    for (let i = 1; i <= 5; i++) { // the variable was not declared
+
+
         console.log(i);
         // 1,2,3,4,5
     }
@@ -42,17 +45,32 @@ function canVote(age) {
 // Example 4 
 
 //What is the output of the following code?
-const isPrime = function (n) {
-    for (let i = 2; i <= Math.sqrt(n); i++) {
+const isPrime = function (n) { // use var --> goblal conflict
+    for (let i = 2; i <= Math.sqrt(n); i++) { // the variable was not declared
         if (n % i == 0) return false;
     }
     return n > 1;
 }
-const sumOfPrimes = function (n) {
+const sumOfPrimes = function (n) { // use var --> global conflict
     let sum = 0;
-    for (let i = 1; i <= n; i++) {
+    for (let i = 1; i <= n; i++) { // the variable was not declared
         if (isPrime(i)) sum += i;
     }
     return sum;
 }
 console.log(sumOfPrimes(10));
+
+// Example 5
+
+const isPerfect = function (number) { // user var --> global conflict
+    let sumOfFactors = 0; // user var --> global conflict
+    for (let index = 1; index <= number; index++) { // the variable was not declared
+        if (number % index === 0) { // use == 
+            sumOfFactors += index;
+        }
+    }
+    return sumOfFactors === number * 2;  // use == 
+};
+for (let i = 1; i <= 10; i++) { // the variable was not declared
+    console.log('is ' + i + ' perfect?: ' + isPerfect(i));
+}
